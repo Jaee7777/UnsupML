c1c3420f011695f0c57cd22c79fa49ce84412c55
Jaee <oh.jaee@northeastern.edu>
2025-01-28 16:49:31 -0500

    hw01

--- src/kmean.py
+++ src/kmean.py
@@ -12,7 +12,7 @@ def find_centroid(df, k, d):
     return centroid
 
 
-def assign_cluster(df, k, centroid):
+def assign_cluster(df, k, d, centroid):
     # initialize a temparory space to store Euclidean distances.
     temp = np.zeros(k)
     for x in range(df.shape[0]):
@@ -26,50 +26,85 @@ def assign_cluster(df, k, centroid):
     return
 
 
-def lloyd(df, k, max_iter=50):
+def lloyd(df, k, max_iter=100):
     # Find the dimension of the datapoints.
     d = df.shape[1]
-
     # randomly assign all points to a centroid/cluster.
     df["k"] = np.random.randint(0, k, size=len(df))
-    print(df)
-
     # Calculate the centroids as mean of their assigned points.
     centroid = find_centroid(df, k, d)
-    for _ in range(max_iter):
+    # initialize while loop.
+    iter = 0
+    dif_centroid = np.ones((k, d))
+    threshold = np.ones((k, d)) * 0
+    while np.any(dif_centroid>threshold) and iter < max_iter:
+        centroid_before = centroid # update centroid_result.
+        iter += 1 # update number of iterations.
         # reassign each datapoint to its closest centroid.
-        assign_cluster(df, k, centroid)
+        assign_cluster(df, k, d, centroid)
         # recalculate the centroid.
         centroid = find_centroid(df, k, d)
-    return centroid, df
+        # check if the centroid has moved.
+        dif_centroid = np.abs(centroid_before-centroid)
+    return centroid, df, iter
 
+def macqueen(df, k, max_iter=100):
+    # Find the dimension of the datapoints.
+    d = df.shape[1]
+    # Find number of datapoints n.
+    n = df.shape[0]
+    # add an empty column for cluster labels.
+    df["k"] = ""
+    # randomly choose k datapoints as centroids.
+    # choose indices of df which are chosen to be the initial centroids.
+    i_rnd = np.random.choice(n, k, replace=False)
+    centroid = np.zeros((k, d))  # initialize dimensions of centroids.
+    for i in range(k):
+        centroid[i,:] = df.iloc[i_rnd[i], :d]
+        df.at[i_rnd[i],"k"] = i # label cluster to initial centroid datapoints.
+    # initialize while loop.
+    iter = 0
+    dif_centroid = np.ones((k, d))
+    threshold = np.ones((k, d)) * 0
+    while np.any(dif_centroid>threshold) and iter < max_iter:
+        centroid_before = centroid # update centroid_result.
+        iter += 1 # update number of iterations.
+        # assign each datapoint to its closest centroid.
+        assign_cluster(df, k, d, centroid)
+        # recalculate the centroid.
+        centroid = find_centroid(df, k, d)
+        # check if the centroid has moved.
+        dif_centroid = np.abs(centroid_before-centroid)
+    return centroid, df, iter
 
-def macqueen(X, k, max_iters=100):
-    """MacQueen's k-means algorithm."""
-
-    # Initialize centroids randomly
-    centroids = X[np.random.choice(X.shape[0], k, replace=False)]
-
-    for _ in range(max_iters):
-        # Assign each data point to the closest centroid
-        assignments = np.array(
-            [np.argmin(np.linalg.norm(X - c, axis=1)) for c in centroids]
-        )
-
-        # Update centroids
-        new_centroids = np.array([X[assignments == i].mean(axis=0) for i in range(k)])
-
-        # Check for convergence
-        if np.allclose(centroids, new_centroids):
-            break
-
-        centroids = new_centroids
-
-    return assignments, centroids
-
-
-def hartigan(k, data):
-    pass
+def hartigan(df, k, max_iter=100):
+    # Find the dimension of the datapoints.
+    d = df.shape[1]
+    # Find number of datapoints n.
+    n = df.shape[0]
+    # randomly assign all points to a centroid/cluster.
+    df["k"] = np.random.randint(0, k, size=len(df))
+    # Calculate the centroids as mean of their assigned points.
+    centroid = find_centroid(df, k, d)
+    # initialize while loop.
+    iter = 0
+    dif_centroid = np.ones((k, d))
+    threshold = np.ones((k, d)) * 0
+    while np.any(dif_centroid>threshold) and iter < max_iter:
+        for x in range(n): # for each datapoint, find closest centroid.
+            df_datapoint = df.iloc[x].copy().to_frame()
+            assign_cluster(df_datapoint, k, d, centroid)    
+        # recalculate the centroids as mean of their assigned points.
+        centroid = find_centroid(df, k, d)
+        centroid_before = centroid # update centroid_result.
+        iter += 1 # update number of iterations.
+        # assign each datapoint to its closest centroid.
+        assign_cluster(df, k, d, centroid)
+        # recalculate the centroid.
+        centroid = find_centroid(df, k, d)
+        # check if the centroid has moved.
+        dif_centroid = np.abs(centroid_before-centroid)
+    return centroid, df, iter
 
 
 if __name__ == "__main__":
@@ -83,29 +118,40 @@ if __name__ == "__main__":
     df = pd.DataFrame(data=iris.data, columns=iris.feature_names)
     # df["target"] = iris.target
 
-    # Find the dimension of the datapoints.
-    d = df.shape[1]
-
     # Fix the seed for the random number generator for reproducibility.
-    np.random.seed(42)
+    #np.random.seed(42)
 
-    centroid, df = lloyd(df, k)
+    # Lloyd Algorithm
+    centroid, df, iter = lloyd(df, k)
     print("Lloyd Algorithm: ")
-    print(df)
+    #print(df)
     print(f"centroiods are: \n{centroid}")
 
     for x in range(k):
         print(f"{x}-cluster has {df[df["k"] == x].shape[0]} points.")
+    print(f"Lloyd Algorithm took {iter} iterations.")
 
+    # Create a dataframe.
+    df = pd.DataFrame(data=iris.data, columns=iris.feature_names)
+    # Mac Queen Algorithm.
+    centroid, df, iter = macqueen(df, k)
+    print("Mac Queen Algorithm: ")
+    #print(df)
+    print(f"centroiods are: \n{centroid}")
 
-"""
-    centroids, assignments = lloyd_algorithm(X, k)
+    for x in range(k):
+        print(f"{x}-cluster has {df[df["k"] == x].shape[0]} points.")
+    print(f"Mac Queen Algorithm took {iter} iterations.")
 
-    print("Centroids:", centroids)
-    print("Assignments:", assignments)
+    # Create a dataframe.
+    df = pd.DataFrame(data=iris.data, columns=iris.feature_names)
+    # Hartigan-Wong Algorithm.
+    centroid, df, iter = hartigan(df, k)
+    print("Hartigan-Wong Algorithm: ")
+    #print(df)
+    print(f"centroiods are: \n{centroid}")
 
-    centroids, assignments = macqueen(X, k)
+    for x in range(k):
+        print(f"{x}-cluster has {df[df["k"] == x].shape[0]} points.")
+    print(f"Hartigan-Wong Algorithm took {iter} iterations.")
 
-    print("Centroids:", centroids)
-    print("Assignments:", assignments)
-"""
